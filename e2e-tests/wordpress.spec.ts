fec1272c0f05ad417dfbb7adb5c1ee3df1bb062a
JOTAKI, Taisuke <jotaki@digitalcube.jp>
2023-04-03 22:14:07 +0900

    update github actions.

--- e2e-tests/wordpress.spec.ts
+++ e2e-tests/wordpress.spec.ts
@@ -21,6 +21,21 @@ test('Enable registration, register a new user, and test email content', async (
   await page.goto('http://localhost:8080/wp-login.php?action=logout');
   await page.click('a[href="http://localhost:8080/wp-login.php?action=logout&_wpnonce=a3b3c3d3e3"]');
 
+  // Go to the WordPress registration page
+  await page.goto('http://localhost:8080/wp-login.php?action=register');
+
+  // Fill out the registration form
+  await page.fill('#user_login', 'testuser');
+  await page.fill('#user_email', 'testuser@example.com');
+  await page.click('#wp-submit');
+
+
+  // Check for successful registration
+  const registrationSuccess = await page.waitForSelector('#login p.message');
+  expect(await registrationSuccess.textContent()).toContain('Registration complete. Please check your email');
+
+  // Take a screenshot of the registration success message
+  await page.screenshot({ path: 'registration_success.png' });
 
   // Give MailHog some time to capture the email
   await new Promise(resolve => setTimeout(resolve, 5000));
@@ -28,17 +43,12 @@ test('Enable registration, register a new user, and test email content', async (
   // Fetch the captured emails from MailHog API
   const response = await fetch('http://localhost:8025/api/v2/messages');
   const data = await response.json();
-  
-  // Assuming there's only one email captured by MailHog
-  const email = data.items[0];
 
-  // Test the email subject
-  expect(email.Content.Headers.Subject[0]).toBe('Expected Subject');
+  // Find the email sent to the test user
+  const email = data.items.find((item: any) => item.To[0].Mailbox === 'testuser' && item.To[0].Domain === 'example.com');
 
-  // Test the email recipient
-  expect(email.Content.Headers.To[0]).toBe('recipient@example.com');
+  // Test the email content
+  expect(email.Content.Headers.Subject[0]).toBe('Your New WordPress Site');
+  expect(email.Content.Headers['X-Mailer'][0]).toBe('PHPMailer 6.1.6 (https://github.com/PHPMailer/PHPMailer)');
 
-  // Test the email body content
-  const emailBody = email.Content.Body;
-  expect(emailBody).toContain('Expected content in email body');
 });
