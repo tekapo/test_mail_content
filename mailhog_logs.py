4086ec3d06cf4c14b1eafe8de06b8e3eadc8b2bb
JOTAKI, Taisuke <jotaki@digitalcube.jp>
2023-04-03 18:23:54 +0900

    update github actions.

---
+++ mailhog_logs.py
@@ -0,0 +1,19 @@
+# mailhog_logs.py
+
+import json
+
+import requests
+
+MAILHOG_API = "http://localhost:8025/api/v2/messages"
+
+response = requests.get(MAILHOG_API)
+messages = response.json()
+
+for msg in messages["items"]:
+    print("===================================")
+    print(f"From: {msg['From']}")
+    print(f"To: {msg['To']}")
+    print(f"Subject: {msg['Content']['Headers']['Subject'][0]}")
+    print("Body:")
+    print(msg["Content"]["Body"])
+    print("===================================\n")
