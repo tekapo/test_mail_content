0cb190e265d95b6c779c3bb0cacf1ad49b38f96a
JOTAKI, Taisuke <jotaki@digitalcube.jp>
2023-04-03 17:24:43 +0900

    update github actions.

--- test_email.py
+++ test_email.py
@@ -6,7 +6,7 @@ def test_email_subject():
     with open("email.json", "r") as file:
         email_data = json.load(file)
 
-    expected_subject = "Your expected email subject"
+    expected_subject = "Test email from WordPress"
     assert email_data["subject"] == expected_subject, f"Expected subject: '{expected_subject}', but got: '{email_data['subject']}'"
 
 
@@ -14,5 +14,5 @@ def test_email_recipient():
     with open("email.json", "r") as file:
         email_data = json.load(file)
 
-    expected_recipient = "example@example.com"
+    expected_recipient = "recipient@example.com"
     assert email_data["to"] == expected_recipient, f"Expected recipient: '{expected_recipient}', but got: '{email_data['to']}'"
