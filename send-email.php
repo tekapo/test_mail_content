a71821c39bffe27ff8b2cf222e81a2d0a2f001cc
JOTAKI, Taisuke <jotaki@digitalcube.jp>
2023-04-03 19:30:25 +0900

    update github actions.

---
+++ send-email.php
@@ -0,0 +1,15 @@
+<?php
+// send-email.php
+
+require_once('wp-load.php');
+
+$to = 'recipient@example.com';
+$subject = 'Test Email';
+$message = 'This is a test email sent from WordPress on GitHub Actions.';
+$headers = 'From: sender@example.com' . "\r\n";
+
+if (wp_mail($to, $subject, $message, $headers)) {
+    echo 'Email sent successfully';
+} else {
+    echo 'An error occurred while sending the email';
+}
